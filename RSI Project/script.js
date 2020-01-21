bd6b3454e84a4903e2f63018e3da2563852fce31
laffed <roarklaffed@gmail.com>
2020-01-21 13:43:24 -0600

    1. Added mobile responsive navigation bar 2. Added burger menu for mobile view that transitions    vertical nav on click

--- RSI Project/script.js
+++ RSI Project/script.js
@@ -1,1 +1,16 @@
 //script for library catalogue
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger-menu");
+  const nav = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+const app = () => {
+  navSlide();
+};
+
+app();
