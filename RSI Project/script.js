9243806bdf4e9ec67ec0a2b638d9af009b5dd246
laffed <roarklaffed@gmail.com>
2020-01-23 14:11:48 -0600

    Added javascript filter function to search bar

--- RSI Project/script.js
+++ RSI Project/script.js
@@ -14,3 +14,23 @@ const app = () => {
 };
 
 app();
+
+function searchFunction() {
+  let input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1]; //search by name. change index for diff search terms
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
