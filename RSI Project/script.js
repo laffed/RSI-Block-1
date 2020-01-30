e4f4435db7f56e44198324156a05b966006c9b6b
laffed <roarklaffed@gmail.com>
2020-01-30 09:20:59 -0600

    Added javascript object database

--- RSI Project/script.js
+++ RSI Project/script.js
@@ -34,3 +34,196 @@ function searchFunction() {
     }
   }
 }
+
+
+//database
+
+let coworkerDB = [{
+    id: "id",
+    firstName: "First Name",
+    LastName: "Last Name",
+    Role: "Role",
+    Notes: "Notes"
+  },
+  {
+    id: "1",
+    firstName: "John",
+    LastName: "Kangsumrith",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "2",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "3",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "4",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "5",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "6",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  }, {
+    id: "7",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "8",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "9",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "10",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "11",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "12",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "13",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "14",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "15",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "16",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "17",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "18",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "19",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "20",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "21",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "22",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "23",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "24",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "25",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  },
+  {
+    id: "26",
+    firstName: "",
+    LastName: "",
+    Role: "",
+    Notes: ""
+  }
+]
