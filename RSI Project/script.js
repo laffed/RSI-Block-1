18e7fa459fff175a2f666b43aaa35ef9838f059d
laffed <roarklaffed@gmail.com>
2020-01-30 10:18:47 -0600

    Filled in javascript array database

--- RSI Project/script.js
+++ RSI Project/script.js
@@ -17,6 +17,7 @@ app();
 
 function searchFunction() {
   let input, filter, table, tr, td, i, txtValue;
+
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
@@ -35,10 +36,10 @@ function searchFunction() {
   }
 }
 
-
 //database
 
-let coworkerDB = [{
+let coworkerDB = [
+  {
     id: "id",
     firstName: "First Name",
     LastName: "Last Name",
@@ -49,154 +50,155 @@ let coworkerDB = [{
     id: "1",
     firstName: "John",
     LastName: "Kangsumrith",
-    Role: "",
-    Notes: ""
+    Role: "Web Dev",
+    Notes: "javascript"
   },
   {
     id: "2",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Leon",
+    LastName: "Love",
+    Role: "Fullstack Web Dev",
+    Notes: "currently painting his house"
   },
   {
     id: "3",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Joshua",
+    LastName: "Waldrup",
+    Role: "LIMS Dev",
+    Notes: "getting into daytrading"
   },
   {
     id: "4",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Reid",
+    LastName: "Williams",
+    Role: "E-Fleet Associate",
+    Notes: "Auburn Grad, Fairhope Jazz"
   },
   {
     id: "5",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Eric",
+    LastName: "Balderson",
+    Role: "Java Dev",
+    Notes: "Whitehat hacker, cyber sec"
   },
   {
     id: "6",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
-  }, {
+    firstName: "Michael",
+    LastName: "Haymon",
+    Role: "Java Dev",
+    Notes: "funny python hater"
+  },
+  {
     id: "7",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Iriel",
+    LastName: "Evans",
+    Role: "HR Intern",
+    Notes: "Lived in NOLA"
   },
   {
     id: "8",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Spencer",
+    LastName: "Radford",
+    Role: "JA Java",
+    Notes: "recent grad, video games"
   },
   {
     id: "9",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Dominic",
+    LastName: "Reed",
+    Role: "JA QA",
+    Notes: "Military, USPS"
   },
   {
     id: "10",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Corey",
+    LastName: "Herington",
+    Role: ".NET Dev",
+    Notes: "game makers guild"
   },
   {
     id: "11",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Blake",
+    LastName: "Johns",
+    Role: "JA",
+    Notes: "enrolled 17chrs"
   },
   {
     id: "12",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Joseph",
+    LastName: "Abreu",
+    Role: "Senior .NET Dev",
+    Notes: "fellow musician"
   },
   {
     id: "13",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Gary",
+    LastName: "Gealy",
+    Role: "Senior Dev",
+    Notes: "sk8tr"
   },
   {
     id: "14",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Chris",
+    LastName: "Whitehead",
+    Role: "Java&.NET Backend",
+    Notes: "null"
   },
   {
     id: "15",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Natascha",
+    LastName: "Thomas",
+    Role: "Super PM",
+    Notes: "cares"
   },
   {
     id: "16",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Patrick",
+    LastName: "Morton",
+    Role: "Principle",
+    Notes: "rubiks cube, monster energy"
   },
   {
     id: "17",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Mark",
+    LastName: "Browning",
+    Role: "Principle",
+    Notes: "manager"
   },
   {
     id: "18",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Savannah",
+    LastName: "Goldman",
+    Role: "Assoc Consultant",
+    Notes: "Check in training"
   },
   {
     id: "19",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Bethany",
+    LastName: "Straughan",
+    Role: "PM",
+    Notes: "check in training"
   },
   {
     id: "20",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Greg",
+    LastName: "Dugger",
+    Role: ".NET Consultant",
+    Notes: "well dressed"
   },
   {
     id: "21",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Hunter",
+    LastName: "Cobb",
+    Role: "Senior .NET",
+    Notes: "Blazer, quite"
   },
   {
     id: "22",
-    firstName: "",
-    LastName: "",
-    Role: "",
-    Notes: ""
+    firstName: "Ariel",
+    LastName: "Burke",
+    Role: "Java Consultant",
+    Notes: "Leon's team"
   },
   {
     id: "23",
@@ -226,4 +228,4 @@ let coworkerDB = [{
     Role: "",
     Notes: ""
   }
-]
+];
